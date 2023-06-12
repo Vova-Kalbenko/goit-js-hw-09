a2bd3ffbbdd678e455e8a7ce7b46d3954a6a35cd
Vova-Kalbenko <lastforhs3@gmail.com>
2023-06-12 21:09:42 +0300

    fix-task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,25 +1,28 @@
+const btnStartEl = document.querySelector('[data-start]');
+const btnStopEl = document.querySelector('[data-stop]');
+btnStopEl.disabled = true;
 
-const btnStartEl =  document.querySelector('[data-start]')
-const btnStopEl =  document.querySelector('[data-stop]')
-btnStopEl.disabled = true
+let switcherID = null;
 
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-  }
-
-const handleStartColorSwitcher =(e)=>{
-    switcherID = setInterval(()=>{
-        btnStopEl.disabled = false
-        document.body.style.backgroundColor = getRandomHexColor()
-        btnStartEl.disabled = true
-    }, 1000)
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
-const handleStopColorSwitcher =(e)=>{
-    clearInterval(switcherID)
-    btnStartEl.disabled = false
-    btnStopEl.disabled = true
-}
+const handleStartColorSwitcher = e => {
+  switcherID = setInterval(() => {
+    btnStopEl.disabled = false;
+    document.body.style.backgroundColor = getRandomHexColor();
+    btnStartEl.disabled = true;
+  }, 1000);
+};
 
-btnStartEl.addEventListener('click', handleStartColorSwitcher)
-btnStopEl.addEventListener('click', handleStopColorSwitcher)
+const handleStopColorSwitcher = e => {
+  clearInterval(switcherID);
+  btnStartEl.disabled = false;
+  btnStopEl.disabled = true;
+};
+
+btnStartEl.addEventListener('click', handleStartColorSwitcher);
+btnStopEl.addEventListener('click', handleStopColorSwitcher);
