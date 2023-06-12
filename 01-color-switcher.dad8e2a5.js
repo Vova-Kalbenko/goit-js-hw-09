65b1376ecdbe426fee4b6185760f9c83fee4ac54
Vova-Kalbenko <122880828+Vova-Kalbenko@users.noreply.github.com>
2023-06-12 13:09:14 +0000

    Deploying to gh-pages from @ Vova-Kalbenko/goit-js-hw-09@cd44468ac2bff739427ffe9be0a5681f36f69776 🚀

---
+++ 01-color-switcher.dad8e2a5.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");t.disabled=!0;e.addEventListener("click",(d=>{switcherID=setInterval((()=>{t.disabled=!1,document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`,e.disabled=!0}),1e3)})),t.addEventListener("click",(d=>{clearInterval(switcherID),e.disabled=!1,t.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.dad8e2a5.js.map
